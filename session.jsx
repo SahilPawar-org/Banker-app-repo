d741ccf9803fd44895a362b74c82e789d4afef12
SahilPawar <61931188+SahilPawar-org@users.noreply.github.com>
2024-06-16 16:28:24 +0530

    basic

---
+++ session.jsx
@@ -0,0 +1,147 @@
+<div>
+  <meta charSet="UTF-8" />
+  <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+  <style dangerouslySetInnerHTML={{__html: ".subcard {\n        max-width: 100px;\n        /* Adjust width as needed */\n        max-height: 100px;\n        /* Adjust height as needed */\n    }\n    \n    /* .card-img-top { */\n        /* width: 80%; */\n        /* Adjust image width */\n        /* height: 80%; */\n        /* Adjust image height */\n        /* max-width: 100px; */\n        /* max-height: 100px; Example: Set a maximum height for the image */\n    /* } */\n    " }} />
+  <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">Navbar</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Features</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Pricing</a>
+          </li>
+          <li className="nav-item" <a aria-disabled="true">Disabled
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div className="container text-center">
+    <div className="row p-2">
+      <div className="col-12 g-2 card ">
+        <div className="row justify-content-center g-2 mt-2 ">
+          <div className="card m-2 subcard">
+            <div className="card-body">
+              <img className="d-flex " src="/user.png" alt="..." height="10px" width="10px" />
+              <img src="/user.png" className="card-img-top" alt="..." />
+            </div>
+          </div>
+          <div className="card m-2 subcard">
+            <div className="card-body">
+              <img src="/user.png" className="card-img-top" alt="..." />
+            </div>
+          </div>
+          <div className="card m-2 subcard">
+            <div className="card-body">
+              <img src="/user.png" className="card-img-top" alt="..." />
+            </div>
+          </div>
+          <div className="card m-2 subcard">
+            <div className="card-body">
+              <img src="/user.png" className="card-img-top" alt="..." />
+            </div>
+          </div>
+          <div className="card m-2 subcard">
+            <div className="card-body">
+              <img src="/user.png" className="card-img-top" alt="..." />
+            </div>
+          </div>
+          <div className="card m-2 subcard">
+            <div className="card-body">
+              <img src="/user.png" className="card-img-top" alt="..." />
+            </div>
+          </div>
+          <div className="card m-2 subcard">
+            <div className="card-body">
+              <img src="/user.png" className="card-img-top" alt="..." />
+            </div>
+          </div>
+          <div className="card m-2 subcard">
+            <div className="card-body">
+              <img src="/user.png" className="card-img-top" alt="..." />
+            </div>
+          </div>
+          <div className="card m-2 subcard">
+            <div className="card-body">
+              <img src="/user.png" className="card-img-top" alt="..." />
+            </div>
+          </div>
+          <div className="card m-2 subcard">
+            <div className="card-body">
+              <img src="/user.png" className="card-img-top" alt="..." />
+            </div>
+          </div>
+          <div className="m-2"><button className="btn btn-primary ">add bank</button> <button className="btn btn-primary ">add bank</button></div>
+        </div>
+      </div>
+      <div className="col-12 g-2 card ">
+        <div className=" m-2 d-flex ">
+          <div className="card col subcard ">
+            <div className="card-body">
+              <img src="/user.png" className="card-img-top" alt="..." />
+            </div>
+          </div>
+          <div className>
+            <button className="btn btn-primary m-2 mt-4">swapp</button>
+            <button className="btn btn-primary m-2  mt-4">request</button>
+          </div>
+        </div>
+        <div className=" m-2 d-flex ">
+          <div className="card col subcard ">
+            <div className="card-body">
+              <img src="/user.png" className="card-img-top" alt="..." />
+            </div>
+          </div>
+          <div className>
+            <button className="btn btn-primary m-2 mt-4">swapp</button>
+            <button className="btn btn-primary m-2  mt-4">request</button>
+          </div>
+        </div>
+        <div className=" m-2 d-flex ">
+          <div className="card col subcard ">
+            <div className="card-body">
+              <img src="/user.png" className="card-img-top" alt="..." />
+            </div>
+          </div>
+          <div className>
+            <button className="btn btn-primary m-2 mt-4">swapp</button>
+            <button className="btn btn-primary m-2  mt-4">request</button>
+          </div>
+        </div>
+        <div className=" m-2 d-flex ">
+          <div className="card col subcard ">
+            <div className="card-body">
+              <img src="/user.png" className="card-img-top" alt="..." />
+            </div>
+          </div>
+          <div className>
+            <button className="btn btn-primary m-2 mt-4">swapp</button>
+            <button className="btn btn-primary m-2  mt-4">request</button>
+          </div>
+        </div>
+        <div className=" m-2 d-flex ">
+          <div className="card col subcard ">
+            <div className="card-body">
+              <img src="/user.png" className="card-img-top" alt="..." />
+            </div>
+          </div>
+          <div className>
+            <button className="btn btn-primary m-2 mt-4">swapp</button>
+            <button className="btn btn-primary m-2  mt-4">request</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div></div>
